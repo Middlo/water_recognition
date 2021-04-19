593655662f021512df3e647a5bba17dfc400ee07
MiddleTv <sebastian_baszcz@yahoo.co.uk>
2021-04-19 20:21:46 +0200

    implemented JSON to Local PNG

--- ReactJs/src/App.js
+++ ReactJs/src/App.js
@@ -17,23 +17,60 @@ const center = {
   lng: -38.523
 };
 
+
+function degrees_to_radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+function radians_to_degrees(radians)
+{
+  var pi = Math.PI;
+  return radians * (180/pi);
+}
+
+
 class MyComponents extends Component {
   state = {markerList:[]}
+
+  calculateCoords = (lat, lng) => {
+    const R = 6378.1
+    const brngA = degrees_to_radians(135)
+    const brngB = degrees_to_radians(315)
+
+    const d = 5
+
+    const φ1 = degrees_to_radians(lat) 
+    const λ1 = degrees_to_radians(lng)
+
+    const φ2 = radians_to_degrees(Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(brngA) ))
+    const λ2 = radians_to_degrees(λ1 + Math.atan2(Math.sin(brngA)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2)))
+    console.log(φ2,λ2)
+
+    const φ3 = radians_to_degrees(Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(brngB) ))
+    const λ3 = radians_to_degrees(λ1 + Math.atan2(Math.sin(brngB)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ3)))
+    console.log(φ3,λ3)
+
+    return ([φ2,λ2,φ3,λ3]) 
+  }
 
   onRightClick = (e) => {
     let latitude = e.latLng.lat()
     let longitude = e.latLng.lng()
-    let coordinates = {latitude, longitude}
+    let coords = this.calculateCoords(latitude, longitude)
+    let coordinates = {latitude, longitude, north: coords[0], east: coords[1], south: coords[2], west:coords[3]}
+
     this.setState(prevState => ({
      markerList: [...prevState.markerList, coordinates]
-
     }))
+    
     console.log(this.state.markerList)
   }
 
   renderMarkers = (list) => {
     return list.map(marker => {
-      return (<MarkerRectangle lat={marker.latitude} lng={marker.longitude}/>)
+      return (<MarkerRectangle lat={marker.latitude} lng={marker.longitude} north={marker.north} east={marker.east} south={marker.south} west={marker.west}/>)
     })
   }
 
--- ReactJs/src/components/MarkerRectangle.js
+++ ReactJs/src/components/MarkerRectangle.js
@@ -1,68 +1,35 @@
 import React, { Component } from 'react';
 import { Marker, Rectangle } from '@react-google-maps/api';
 
-function degrees_to_radians(degrees)
-{
-  var pi = Math.PI;
-  return degrees * (pi/180);
-}
-
-function radians_to_degrees(radians)
-{
-  var pi = Math.PI;
-  return radians * (180/pi);
-}
-
 export default class MarkerRectangle extends Component{
     constructor(props){
         super(props)
-        this.bounds = {}
     }
 
     state={
         position : {
             lat: this.props.lat,
             lng: this.props.lng
+        },
+        bounds : {
+            north: this.props.north,
+            east: this.props.east,
+            south: this.props.south,
+            west: this.props.west
         }    
     }
 
-    calculateCoords = (lat, lng) => {
-        const R = 6378.1
-        const brngA = degrees_to_radians(135)
-        const brngB = degrees_to_radians(315)
 
-        const d = 5
-
-        const φ1 = degrees_to_radians(lat) 
-        const λ1 = degrees_to_radians(lng)
-
-        const φ2 = radians_to_degrees(Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(brngA) ))
-        const λ2 = radians_to_degrees(λ1 + Math.atan2(Math.sin(brngA)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2)))
-        console.log(φ2,λ2)
-
-        const φ3 = radians_to_degrees(Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(brngB) ))
-        const λ3 = radians_to_degrees(λ1 + Math.atan2(Math.sin(brngB)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ3)))
-        console.log(φ3,λ3)
-
-        return ([φ2,λ2,φ3,λ3]) 
-    }
-
-    RectangleCoords = () => {
-        let coords = this.calculateCoords(this.props.lat,this.props.lng)
-        let bounds = {north: coords[0], east: coords[1], south: coords[2], west:coords[3]}
-        this.bounds = bounds
-    }
 
     onClick = () => {
         //REMOVE
     }
 
     render(){
-        this.RectangleCoords()
         return(
             <div>
                 <Marker clickable onClick={() => this.onClick()}position={this.state.position}/>  
-                <Rectangle bounds={this.bounds}/>            
+                <Rectangle bounds={this.state.bounds}/>            
             </div>
         )
     }
