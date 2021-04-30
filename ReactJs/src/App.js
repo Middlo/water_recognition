9bf8d8011e4eeaadaef3acfb384594c29e66a8cd
MiddleTv <sebastian_baszcz@yahoo.co.uk>
2021-04-30 21:08:42 +0200

    implemented several cleaning and extracting scripts

--- ReactJs/src/App.js
+++ ReactJs/src/App.js
@@ -17,60 +17,23 @@ const center = {
   lng: -38.523
 };
 
-
-function degrees_to_radians(degrees)
-{
-  var pi = Math.PI;
-  return degrees * (pi/180);
-}
-
-function radians_to_degrees(radians)
-{
-  var pi = Math.PI;
-  return radians * (180/pi);
-}
-
-
 class MyComponents extends Component {
   state = {markerList:[]}
-
-  calculateCoords = (lat, lng) => {
-    const R = 6378.1
-    const brngA = degrees_to_radians(135)
-    const brngB = degrees_to_radians(315)
-
-    const d = 5
-
-    const φ1 = degrees_to_radians(lat) 
-    const λ1 = degrees_to_radians(lng)
-
-    const φ2 = radians_to_degrees(Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(brngA) ))
-    const λ2 = radians_to_degrees(λ1 + Math.atan2(Math.sin(brngA)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2)))
-    console.log(φ2,λ2)
-
-    const φ3 = radians_to_degrees(Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(brngB) ))
-    const λ3 = radians_to_degrees(λ1 + Math.atan2(Math.sin(brngB)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ3)))
-    console.log(φ3,λ3)
-
-    return ([φ2,λ2,φ3,λ3]) 
-  }
 
   onRightClick = (e) => {
     let latitude = e.latLng.lat()
     let longitude = e.latLng.lng()
-    let coords = this.calculateCoords(latitude, longitude)
-    let coordinates = {latitude, longitude, north: coords[0], east: coords[1], south: coords[2], west:coords[3]}
-
+    let coordinates = {latitude, longitude}
     this.setState(prevState => ({
      markerList: [...prevState.markerList, coordinates]
+
     }))
-    
     console.log(this.state.markerList)
   }
 
   renderMarkers = (list) => {
     return list.map(marker => {
-      return (<MarkerRectangle lat={marker.latitude} lng={marker.longitude} north={marker.north} east={marker.east} south={marker.south} west={marker.west}/>)
+      return (<MarkerRectangle lat={marker.latitude} lng={marker.longitude}/>)
     })
   }
 
@@ -83,10 +46,18 @@ class MyComponents extends Component {
     this.setState({markerList: data})
   }
 
+  onUndo = () => {
+    var array = [...this.state.markerList]
+    array.pop()
+    this.setState({
+      markerList: array
+    })
+  }
+
   render() {
     return (
       <LoadScript
-        googleMapsApiKey="AIzaSyAOCjHQ-RoEgcmpP_PZzdmwjWEZ5wBwnd4"
+        googleMapsApiKey="AIzaSyACuqekmPYqHlh_H84w8CwM2xHWDozrfnk"
       >
         <GoogleMap
           mapContainerStyle={containerStyle}
@@ -98,6 +69,8 @@ class MyComponents extends Component {
         </GoogleMap>
         <button className={"ButtonSave"} onClick={() => this.onSave()}>SAVE</button>
         <button className={"ButtonLoad"} onClick={() => this.onLoad()}>LOAD</button>
+        <button className={"ButtonUndo"} onClick={() => this.onUndo()}>UNDO</button>
+        <p className={"CounterTxt"}>Amount of Markers: {this.state.markerList.length}</p>
       </LoadScript>
     )
   }
